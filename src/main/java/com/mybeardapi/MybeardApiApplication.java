538f569c6d173fdd53901a1f3a56301bd18e4bf9
Anderson Carvalho <anderson.cvlh@gmail.com>
2017-11-29 13:11:10 -0300

    Inicio

---
+++ src/main/java/com/mybeardapi/MybeardApiApplication.java
@@ -0,0 +1,12 @@
+package com.mybeardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybeardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybeardApiApplication.class, args);
+	}
+}
