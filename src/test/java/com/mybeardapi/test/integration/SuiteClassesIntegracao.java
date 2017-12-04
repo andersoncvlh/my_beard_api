27643401b6badb00aa43a3ae1e14f57f41453ca9
Anderson Carvalho Martins <anderson.cvlh@gmail.com>
2017-12-04 13:40:23 -0200

    Suite de testes de integração

--- src/test/java/com/mybeardapi/test/integration/SuiteClassesIntegracao.java
+++ src/test/java/com/mybeardapi/test/integration/SuiteClassesIntegracao.java
@@ -1,5 +1,13 @@
 package com.mybeardapi.test.integration;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	TestBarbeariaResource.class
+})
 public class SuiteClassesIntegracao {
 
 }
