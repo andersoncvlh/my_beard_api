2def1070759966408b7585a118d351a51d0e630e
Anderson Carvalho Martins <anderson.cvlh@gmail.com>
2017-12-08 16:03:38 -0200

    Configurando classe para leitura do application properties

--- src/main/java/com/mybeardapi/MybeardApiApplication.java
+++ src/main/java/com/mybeardapi/MybeardApiApplication.java
@@ -2,8 +2,12 @@ package com.mybeardapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.mybeardapi.config.properties.MybeardApiProperty;
 
 @SpringBootApplication
+@EnableConfigurationProperties(MybeardApiProperty.class)
 public class MybeardApiApplication {
 
 	public static void main(String[] args) {
